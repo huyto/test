4bc91abb902623c6a3ee8e0872e8085b63b60c06
hto <huy_to@intuit.com>
2013-04-03 15:30:33 -0400

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "myface"
+maintainer       "hto"
+maintainer_email "huy_to@intuit.com"
+license          "All rights reserved"
+description      "Installs/Configures myface"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
