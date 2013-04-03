c15adbc2070fd44deb2f484ef8d782390222193c
hto <huy_to@intuit.com>
2013-04-03 16:27:31 -0400

    update mysql recipe

--- cookbooks/create_mysql_db/recipes/default.rb
+++ cookbooks/create_mysql_db/recipes/default.rb
@@ -1,13 +1,1 @@
-bash "really awesome way to create a mysql database from chef using the bash method" do
-
-  # dont if the db already exists
-  not_if("/usr/bin/mysql -uroot -pmiller_highlife_lol_jk -e'show databases' | grep #{node[:create_mysql_db][:db_name]}", :user => 'hto')
-
-  # run as the hto user
-  user "hto"
-
-  # a heredoc of the code to execute, note the node hash is created from the JSON file
-  code <<-HEY_BRO_EOM
-  mysql -uroot -ppmiller_highlife_lol_jk -e 'create database #{node[:create_mysql_db][:db_name]}'
-  HEY_BRO_EOM
-end
+log("Create MySQL") { level :info }
